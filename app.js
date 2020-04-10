1e96837da4ab3515fd3086253c21aba3de58aa88
Kadori <56588178+Kadori-Komi@users.noreply.github.com>
2020-04-10 12:06:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+function game(userChoice) {
+    console.log(poo + userChoice);
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+
+    rock_div.addEventListener('click', function() {
+        game("p");
+    })
+
+    rock_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+
+main();
